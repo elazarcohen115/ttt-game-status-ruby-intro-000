6868eabf538f89edbd806afa49282d03af915a7d
elazar cohen <escohen115@gmail.com>
2020-04-08 22:57:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,7 +34,7 @@ def draw? (board)
 end
 
 def over? (board)
-  if won?(board) == true || full?(board) == true || draw?(board) == true
+  if won?(board) != false || full?(board) == true || draw?(board) == true
     return true
   else
     return false
