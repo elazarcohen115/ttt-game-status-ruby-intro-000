350846514e42c70fe4075adaaeccbc4885684d41
elazar cohen <escohen115@gmail.com>
2020-04-08 18:54:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,11 +10,11 @@ def won?(board)
    win_1 = win_combination[0]
    win_2 = win_combination[1]
    win_3 = win_combination[2]
-     if board[win_1] == "X" && board[win_2] == "X" && board[win_3] == "X"
-       return win_combination
-    elsif board[win_1] == "O" && board[win_2] == "O" && board[win_3] == "O"
-    return win_combination
-    else
+   if board[win_1] == "X" && board[win_2] == "X" && board[win_3] == "X"
+     return win_combination
+  elsif board[win_1] == "O" && board[win_2] == "O" && board[win_3] == "O"
+  return win_combination
+  else
       return false
      end
   end
